c6dd54de16f9ea75b67f11abce70a8c8b5b5daee
Juan Carlos del Valle <juan@x8bit.com>
2017-12-30 20:28:11 -0600

    cambios...

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -24,7 +24,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO Specify total number of available coins
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(10000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(10000000000000000000);
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "x8coin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000126029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f1cf31de5ae83d7d5d7fc0a48c4e4d7707350868ed60b5708e492af05a329450";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001fac484c69cd608029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210134bced083c2411c2069011aebf5a8bd99f559aa082e8a3fef23dd3c0f7dc29ae";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
